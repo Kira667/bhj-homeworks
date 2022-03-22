1bbc88e79aaa6771ebeeb5133805db3cb7db45fb
Kira667 <kkg80@yandex.ru>
2022-03-22 11:59:52 +0200

    todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,31 @@
+const tasksFormNode = document.querySelector('#tasks__form');
+const taskInputNode = document.querySelector('#task__input');
+const tasksListNode = document.querySelector('#tasks__list');
+
+
+
+tasksFormNode.addEventListener('submit', (e) => {
+	const taskNode = document.createElement('div');
+	taskNode.classList.add('task');
+
+	const taskTitleNode = document.createElement('div');
+	taskTitleNode.classList.add('task__title');
+	taskTitleNode.innerText = taskInputNode.value;
+
+	const taskRemoveNode = document.createElement('div');;
+	taskRemoveNode.classList.add('task__remove');
+	taskRemoveNode.innerHTML = '&times;';
+
+	taskRemoveNode.addEventListener('click', deleteNode);
+
+	taskNode.appendChild(taskTitleNode);
+	taskNode.appendChild(taskRemoveNode);
+
+	tasksListNode.prepend(taskNode);
+
+});
+
+function deleteNode(event) {
+	event.target.parentElement.remove();
+}
+
